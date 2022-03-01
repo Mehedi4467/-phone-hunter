00da16ca3ae411eb73abd7e9cea0a69317f7964c
Mehedi4467 <mehedihassan4467@gmail.comm>
2022-03-01 06:50:10 +0600

    display phone done

--- js/main.js
+++ js/main.js
@@ -8,12 +8,35 @@ const loadPhoneApi = (inputValue) => {
 
 //display phone data
 const displayPhoneData = (phoneData) => {
+    const displayPhone = document.getElementById('display-phone');
+    displayPhone.textContent = '';
 
     if (phoneData.status === false) {
         displayHideShow('notFound', 'flex');
     } else {
         displayHideShow('notFound', 'none');
+        phoneData.data.forEach(phones => {
+            console.log(phones);
+            const div = document.createElement('div');
+            div.classList.add('col-12', 'col-md-4');
+            div.innerHTML = `
+            <div class="shadow-lg rounded-3 p-4">
+            <div class="d-flex justify-content-center align-items-center">
+                <img src="${phones.image}" alt="${phones.phone_name} image">
+            </div>
+            <div class="text-center mt-3">
+                <h3 class="text-primary">${phones.phone_name}</h3>
+                <h5>Brand : <span>${phones.brand}</span></h5>
+                <button class="btn btn-primary mt-2" data-bs-toggle="modal" data-bs-target="#${phones.slug}">Details</button>
+            </div>
+        </div>
+            `;
+
+            displayPhone.appendChild(div);
+        });
+
     }
+    displayHideShow('spinner', 'none');
 };
 
 // display hide and show style 
@@ -26,6 +49,7 @@ const searchButton = document.getElementById('search-btn');
 searchButton.addEventListener('click', () => {
     const searchInput = document.getElementById('search-input');
     const searchInputValue = searchInput.value;
+    displayHideShow('spinner', 'flex');
     loadPhoneApi(searchInputValue.toLowerCase());
 
     searchInput.value = '';
